4d38daa67e74fd22031a25b144c14de5d7ba0efb
Derek Sivers <derek@sivers.org>
2015-01-06 15:09:02 +1300

    tests for errors

--- errors/test.rb
+++ errors/test.rb
@@ -1,5 +1,6 @@
 require 'pg'
 require 'minitest/autorun'
+require 'json'
 
 DB = PG::Connection.new(dbname: 'sivers', user: 'sivers')
 SQL = File.read('sql.sql')
@@ -14,8 +15,47 @@ Minitest.after_run do
 end
 
 class SqlTest < Minitest::Test
-	def test_update
+	def test_named_constraint
 		res = DB.exec_params("SELECT mime, js FROM update_country($1, $2)", [1, '{"code": "tH"}'])
+		assert_equal 'application/problem+json', res[0]['mime']
+		js = JSON.parse(res[0]['js'])
+		assert js['type'].include? '23514' # check_violation
+		assert js['title'].include? 'charcode'
+		assert js['detail'].include? 'update_country'
+	end
+
+	def test_foreign_key
+		res = DB.exec_params("SELECT mime, js FROM update_city($1, $2)", [1, '{"country_id": 9}'])
+		assert_equal 'application/problem+json', res[0]['mime']
+		js = JSON.parse(res[0]['js'])
+		assert js['type'].include? '23503' # foreign_key_violation
+		assert js['title'].include? 'cities_country_id_fkey'
+		assert js['detail'].include? 'not present in table'
+	end
+
+	def test_column_type
+		res = DB.exec_params("SELECT mime, js FROM update_country($1, $2)", [1, '{"sqkm": "a"}'])
+		assert_equal 'application/problem+json', res[0]['mime']
+		js = JSON.parse(res[0]['js'])
+		assert js['type'].include? '22P02' # invalid_text_representation
+		assert js['title'].include? 'syntax for integer'
+		assert js['detail'].include? 'update_country'
+	end
+
+	def test_404_get
+		res = DB.exec("SELECT mime, js FROM get_country(99)")
+		assert_equal 'application/problem+json', res[0]['mime']
+		js = JSON.parse(res[0]['js'])
+		assert_equal 'Not Found', js['title']
+		assert_equal 404, js['status']
+	end
+
+	def test_404_update
+		res = DB.exec_params("SELECT mime, js FROM update_country($1, $2)", [9, '{"name": "Belgium"}'])
+		assert_equal 'application/problem+json', res[0]['mime']
+		js = JSON.parse(res[0]['js'])
+		assert_equal 'Not Found', js['title']
+		assert_equal 404, js['status']
 	end
 end
 
