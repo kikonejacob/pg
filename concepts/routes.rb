102c8da9b65627da1a270390bea5b9d25a0ff5b1
Derek Sivers <derek@sivers.org>
2015-01-29 08:05:21 +1300

    helper to make API queries even more succinct

--- concepts/routes.rb
+++ concepts/routes.rb
@@ -1,10 +1,13 @@
 require 'sinatra/base'
 require 'pg'
-require 'json'
 DB = PG::Connection.new(dbname: 'sivers', user: 'sivers')
 
 class ConceptsAPI < Sinatra::Base
-	
+
+	def qry(sql, params=[])
+		@res = DB.exec_params('select mime, js from ' + sql, params)
+	end
+
 	after do
 		content_type @res[0]['mime']
 		body @res[0]['js']
@@ -18,47 +21,47 @@ class ConceptsAPI < Sinatra::Base
 	end
 
 	get %r{\A/concepts/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM get_concept($1)", [id])
+		qry('get_concept($1)', [id])
 	end
 	
 	post '/concepts' do
-		@res = DB.exec_params("SELECT * FROM create_concept($1)", [params[:concept]])
+		qry('create_concept($1)', [params[:concept]])
 	end
 	
 	put %r{\A/concepts/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM update_concept($1, $2)", [id, params[:concept]])
+		qry('update_concept($1, $2)', [id, params[:concept]])
 	end
 	
 	delete %r{\A/concepts/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM delete_concept($1)", [id])
+		qry('delete_concept($1)', [id])
 	end
 
 	post %r{\A/concepts/([0-9]+)/tag\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM tag_concept($1, $2)", [id, params[:tag]])
+		qry('tag_concept($1, $2)', [id, params[:tag]])
 	end
 	
 	get '/concepts/tag' do
-		@res = DB.exec_params("SELECT * FROM concepts_tagged($1)", [params[:tag]])
+		qry('concepts_tagged($1)', [params[:tag]])
 	end
 	
 	get %r{\A/pairings/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM get_pairing($1)", [id])
+		qry('get_pairing($1)', [id])
 	end
 	
 	post '/pairings' do
-		@res = DB.exec("SELECT * FROM create_pairing()")
+		qry('create_pairing()')
 	end
 	
 	put %r{\A/pairings/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM update_pairing($1, $2)", [id, params[:thoughts]])
+		qry('update_pairing($1, $2)', [id, params[:thoughts]])
 	end
 	
 	delete %r{\A/pairings/([0-9]+)\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM delete_pairing($1)", [id])
+		qry('delete_pairing($1)', [id])
 	end
 
 	post %r{\A/pairings/([0-9]+)/tag\Z} do |id|
-		@res = DB.exec_params("SELECT * FROM tag_pairing($1, $2)", [id, params[:tag]])
+		qry('tag_pairing($1, $2)', [id, params[:tag]])
 	end
 	
 end
